a3a25ed1de759726ab1a671f6a1804c161eeca3c
Gabriel Herrero <39440714+gabriel-herrero@users.noreply.github.com>
2018-06-04 15:54:02 -0400

    Add files via upload

---
+++ Models/FixedOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADESCOM.Models
+{
+    public class FixedOption
+    {
+        public object Option { get; set; }
+        public string Description { get; set; }
+    }
+}
